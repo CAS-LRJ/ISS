6628484910d3b2185d30b6bb6968013ebb263a76
Meilin Zhu <martial0296@gmail.com>
2023-08-11 10:56:36 +0800

    add carla-sensor base classes

--- ISS/algorithms/sensors/setup.py
+++ ISS/algorithms/sensors/setup.py
@@ -2,4 +2,6 @@ from setuptools import Extension
 
 extensions = [Extension('camera', ['camera.pyx']),
               Extension('carla_camera', ['carla_camera.pyx']),
-              Extension('carla_lidar', ['carla_lidar.pyx'])]
+              Extension('carla_lidar', ['carla_lidar.pyx']),
+              Extension('carla_sensor', ['carla_sensor.pyx']),
+              Extension('carla_actor', ['carla_actor.pyx'])]
