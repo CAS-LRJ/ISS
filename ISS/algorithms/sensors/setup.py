54e8092c0f48b95943e52608b8c63f5ec0257194
Meilin Zhu <martial0296@gmail.com>
2023-08-12 10:22:34 +0800

    add other classes of carla

--- ISS/algorithms/sensors/setup.py
+++ ISS/algorithms/sensors/setup.py
@@ -4,4 +4,8 @@ extensions = [Extension('camera', ['camera.pyx']),
               Extension('carla_camera', ['carla_camera.pyx']),
               Extension('carla_lidar', ['carla_lidar.pyx']),
               Extension('carla_sensor', ['carla_sensor.pyx']),
-              Extension('carla_actor', ['carla_actor.pyx'])]
+              Extension('carla_actor', ['carla_actor.pyx']),
+              Extension('carla_radar', ['carla_radar.pyx']),
+              Extension('carla_vehicle', ['carla_vehicle.pyx']),
+              Extension('carla_infrastructure', ['carla_infrastructure.pyx']),
+              ]
