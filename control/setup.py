065a4517edb7217c5a01c4394adcc43cc00724ab
SailorBrandon <shaohang.han@outlook.com>
2023-12-15 17:09:07 +0800

    init

--- control/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup(
-    packages=['wpt_tracker'],
-    package_dir={'': 'scripts'}
-)
-
-setup(**d)
--- planning/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup(
-    packages=['global_planner', 'local_planner', 'planning_utils', 'motion_predictor'],
-    package_dir={'': 'scripts'}
-)
-
-setup(**d)
