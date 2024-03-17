fix: angle in ekf and flipped the sign of control.steer

ISS/algorithms/state_estimation/ekf/ekf.py
@@ -16,18 +16,23 @@ class EKF:
         self._G = None
         self._is_initialized = False
 
-    def initialize(self, obs_x, obs_y, compass, speed, acc_x, x_std, y_std, compass_std, speed_std, acc_x_std):
-        self._state = np.array([obs_x, obs_y, compass, speed, acc_x])
-        self._Q = np.diag([x_std**2, y_std**2, compass_std**2, speed_std**2, acc_x_std**2])
+    def initialize(self, obs_x, obs_y, obs_yaw, speed, acc_x, x_std, y_std, obs_yaw_std, speed_std, acc_x_std):
+        self._state = np.array([obs_x, obs_y, obs_yaw, speed, acc_x])
+        self._Q = np.diag([x_std**2, y_std**2, obs_yaw_std**2, speed_std**2, acc_x_std**2])
         self._is_initialized = True
     
     def is_initialized(self):
         return self._is_initialized
     
-    def step(self, acc_x, compass, obs_x, obs_y, speed, steer):
+    def step(self, acc_x, obs_yaw, obs_x, obs_y, speed, steer):
         assert self._is_initialized, "EKF is not initialized"
+        obs_yaw = pi_2_pi(obs_yaw - self._state[2]) + self._state[2]
         # Update the state with the bicycle model
+        # print("previous angle: ", self._state[2])
         self.bicycle_model_step(acc_x, steer)
+        # print("steer: ", steer)
+        # print("predicted angle: ", self._state[2])
+        # print("observed angle: ", obs_yaw)
         if DEBUG:
             print("-------------------")
             print("predicted state: ", self._state)
@@ -38,11 +43,13 @@ class EKF:
         # Calculate the Kalman Gain
         K = self._SIGMA @ self._H.T @ np.linalg.inv(self._H @ self._SIGMA @ self._H.T + self._Q)
         # Update the state with the new measurements
-        z = np.array([obs_x, obs_y, compass, speed, acc_x])
+        z = np.array([obs_x, obs_y, obs_yaw, speed, acc_x])
         if DEBUG:
             print("observed state: ", z)
+        # print("added term: ", (K @ (z - self._H @ self._state))[2])
         self._state = self._state + K @ (z - self._H @ self._state)
         self._state[2] = pi_2_pi(self._state[2])
+        # print("updated angle: ", self._state[2])
         # Update the error covariance
         self._SIGMA = (np.eye(5) - K @ self._H) @ self._SIGMA
         if DEBUG:
@@ -70,7 +77,7 @@ class EKF:
         L = self._vehicle_info['wheelbase']
         self._state[0] += v * np.cos(theta) * dt
         self._state[1] += v * np.sin(theta) * dt
-        self._state[2] += pi_2_pi(v * np.tan(steer) / L * dt)
+        self._state[2] += (v * np.tan(steer) / L * dt)
         self._state[3] += acc_x * dt
         # Assuming constant acceleration within this step
         self._state[4] = acc_x

ros1_ws/src/carla_bridge/scripts/carla_bridge/carla_bridge_sensors_node.py
@@ -94,9 +94,11 @@ class CARLABridgeNode:
             self._num_frames += 1
             self._world.tick()
             return
+        
         control = self._controller_bridge.get_control()
-        MAX_STEER_ANGLE = 70 # TODO
-        steering_angle = control.steer * np.deg2rad(MAX_STEER_ANGLE)
+        MAX_STEER_ANGLE = 40 # TODO
+        steering_angle = -control.steer * np.deg2rad(MAX_STEER_ANGLE) # note the negative sign
+        
         self._state_estimator.run_step(data, steering_angle)
         if self._set_global_plan_perception:
             control_command, det, other_cast_locs, other_cast_cmds = self._agent.run_step(data, None)

ros1_ws/src/carla_bridge/scripts/carla_bridge/state_estimator.py
@@ -11,7 +11,7 @@ class GTStateEstimator:
     def __init__(self, vehicle) -> None:
         self._vehicle = vehicle
         self._state_estimation_pub = rospy.Publisher(rospy.get_param("ego_state_topic"), State, queue_size=1)
-        gt_state_estimation_frequency = rospy.get_param('gt_state_estimation_frequency', 10)
+        # gt_state_estimation_frequency = rospy.get_param('gt_state_estimation_frequency', 10)
         # self._timer = rospy.Timer(rospy.Duration(1 / gt_state_estimation_frequency), self.publish_ego_state)
     
     def publish_ego_state(self, event):
@@ -29,12 +29,12 @@ class GTStateEstimator:
         state.heading_angle = pi_2_pi(-np.deg2rad(vehicle_rotation.yaw))
         state.velocity = np.hypot(vehicle_velocity.x, vehicle_velocity.y)
         state.acceleration = np.hypot(vehicle_acceleration.x, vehicle_acceleration.y)
-        # print("real state: ", state)
+        # print("real angle: ", pi_2_pi(-np.deg2rad(vehicle_rotation.yaw)))
+        # print("-------------------")
         self._state_estimation_pub.publish(state)
     
-    def shutdown(self):
-        self._timer.shutdown()
-        self._state_estimation_pub.unregister()
+    def run_step(self, input_data, steer):
+        self.publish_ego_state(None)
     
 class EKFStateEstimator:
     def __init__(self) -> None:
@@ -66,7 +66,7 @@ class EKFStateEstimator:
         obs_acc = imu[0]
         
         if self._ekf.is_initialized() is False:
-            self._ekf.initialize(obs_x, obs_y, obs_yaw, spd, obs_acc, 1e-2, 1e-2, 1e-1, 1e-5, 1e-5)
+            self._ekf.initialize(obs_x, obs_y, obs_yaw, spd, obs_acc, 1e-1, 1e-1, 1e-3, 1e-5, 1e-5)
         else:
             self._ekf.step(obs_acc, obs_yaw, obs_x, obs_y, spd, steer)
         